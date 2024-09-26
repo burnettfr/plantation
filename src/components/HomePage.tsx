5f48f1c961c57f8a50b5849247e6f3a40adb5a55
burnettfr <burnettfr@gmail.com>
2024-09-26 16:53:41 -0400

    Install chakra

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Link to="/second-page">Go to the Second Page</Link>
-    </div>
+    <Grid templateAreas={`"top" "main"`}>
+      <GridItem area="top">Top of Page</GridItem>
+      <GridItem area="main">
+        <h1>Welcome to the Home Page</h1>
+        <Link to="/second-page">Go to the Second Page</Link>
+      </GridItem>
+    </Grid>
   );
 }
 
