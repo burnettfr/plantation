0ea0637e6c1d1d85fcd9edaf6e39a308e7259f98
burnettfr <burnettfr@gmail.com>
2024-09-27 10:56:11 -0400

    Convert pdf files to images, dispaly one image

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { Image } from "@chakra-ui/react";
 
 function SecondPage() {
   return (
     <div>
       <h1>This is the Second Page</h1>
+      <Image src="/Bailey.png" boxSize="600px" />
       <Link to="/">Go to Home Page</Link>
     </div>
   );
